30d32696cc5b6479ae04d73a9c6b43b298607182
Nikola Peric <nikolaperic457@gmail.com>
2026-02-16 11:56:21 +0100

    Dokerizacija

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api', // Laravel radi na portu 8000
+    baseURL: "http://localhost:8000/api", 
 });
 
 // Ovo dodaje token u zaglavlje svakog zahteva ako on postoji u memoriji browsera
